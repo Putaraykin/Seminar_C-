b4d97f53fe07b85d2c36b37f14e392b90a65640a
Andrey Putaraykin <Aput@bk.ru>
2022-08-10 17:14:46 +0300

    Программа удаления второй цифры в введенном трехзначном числе готова

---
+++ HW02_Ex001_OutputSecondDigit/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа. 456 -> 5
+int InputNumber = ReadInt("Введите трехзначное число: ");
+{
+int ReadInt(string message);
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+int FindSecondNumber(int number)
+{
+int firstDigit = InputNumber / 100;
+int lastDigit = InputNumber % 10;
+return (InputNumber - (firstDigit * 100 + lastDigit)) / 10;
+}
+{
+    Console.WriteLine (FindSecondNumber(InputNumber));
+}
