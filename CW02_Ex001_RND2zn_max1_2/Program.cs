8caabb357db37801acdd3db2664347ea6b028eb1
Andrey Putaraykin <Aput@bk.ru>
2022-08-10 13:53:33 +0300

    Исправил доп.задания, потренировался на классной работе

---
+++ CW02_Ex001_RND2zn_max1_2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+int randomNumber = new Random().Next(10, 100);
+Console.WriteLine(randomNumber);
+int firstDigit = randomNumber / 10;
+int secondDigit = randomNumber % 10;
+if(firstDigit > secondDigit)
+{
+Console.WriteLine(firstDigit);
+}
+else
+{
+Console.WriteLine(secondDigit);
+}
