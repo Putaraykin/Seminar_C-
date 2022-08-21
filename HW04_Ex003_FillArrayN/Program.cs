1491040847124ca585213920281647c8d768a5a1
Andrey Putaraykin <Aput@bk.ru>
2022-08-21 20:14:46 +0300

    ДЗ_4 Программа создания и печати массива из N элементов готова

---
+++ HW04_Ex003_FillArrayN/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 29: Напишите программу, которая задаёт 
+// массив из N элементов и выводит их на экран. 5 -> [1, 2, 5, 7, 19]  3 -> [6, 1, 33]
+Console.WriteLine ("Введите размер массива:");
+int Size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[Size];
+FillArrayRandomNumbres(numbers);
+PrintArray(numbers);
+void FillArrayRandomNumbres(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 50);
+    }
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
