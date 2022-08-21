44a68d252afde9c4b4e941fc6945c047d7f53121
Andrey Putaraykin <Aput@bk.ru>
2022-08-21 19:46:49 +0300

    ДЗ_4 Программа суммы цифр в числе готова

---
+++ HW04_Ex002_SumDigit/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе. 452 -> 11   82 -> 10
+int SumDigit=0;
+Console.WriteLine ("Введите число:");
+String? NumberString = (Console.ReadLine());
+for (int i=0; i < NumberString.Length; i++)
+    {
+      string DigitCount = Convert.ToString(NumberString[i]);
+      SumDigit = SumDigit + Convert.ToInt32(DigitCount); 
+      }
+Console.WriteLine ($"Сумма цифр числа {NumberString} => {SumDigit}");
