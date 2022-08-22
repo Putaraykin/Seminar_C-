4e299f5139673a4479b21ca6d0552eaa7adeac04
Andrey Putaraykin <Aput@bk.ru>
2022-08-22 08:03:36 +0300

    ДЗ_5 Программа поиска разницы между максимальным и минимальным элементом массива вещественных чисел с упрощенным заполнением готова

---
+++ HW05_Ex003_Array_Diff_Max_Min/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между 
+// максимальным и минимальным элементами массива. [3 7 22 2 78] -> 76
+int Size = 4;
+double[] numbers = new double[Size];
+FillArrayRandomNumbres(numbers);
+PrintArray(numbers);
+double Min = numbers[0];
+double Max = numbers[0];
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i]<Min)
+    Min = numbers[i];
+}
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i]>Max)
+    Max = numbers[i];
+}
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива равна {Max - Min}");
+void FillArrayRandomNumbres(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-20, 20)+0.12;      
+    }
+}
+void PrintArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
