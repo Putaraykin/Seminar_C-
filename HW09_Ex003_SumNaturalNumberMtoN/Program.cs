da36f4b585b4a77d431d3b14f7d55ec450167bd7
Andrey Putaraykin <Aput@bk.ru>
2022-09-05 16:47:09 +0300

    ДЗ_9 Программа вывода суммы элементов (натуральных чисел) в диапазоне  от M до N готова

---
+++ HW09_Ex003_SumNaturalNumberMtoN/Program.cs
@@ -0,0 +1,29 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+Console.WriteLine("Введите положительное число N:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите положительное число M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+if (numberN > 0 && numberM > 0)
+{
+    Console.WriteLine($"Сумма элементов в диапазоне натуральных чисел от {numberN} до {numberM}:");
+    Console.Write(SumNaturalnumber(numberN, numberM));
+    int SumNaturalnumber(int numN, int numM)
+    {
+        if (numberN > numberM)
+        {
+            if (numN == numM)
+                return numM;
+            return numN + SumNaturalnumber(numN - 1, numM);
+        }
+        else
+        {
+            if (numN == numM)
+                return numM;
+            return numN + SumNaturalnumber(numN + 1, numM);
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Введено не положительное число");
+}
