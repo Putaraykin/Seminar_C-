76f535bd9c6aad2b86dec63c9580ccee8e00fdee
Andrey Putaraykin <Aput@bk.ru>
2022-09-05 15:37:10 +0300

    ДЗ_9 Программа вывода натуральных чисел от M до N готова

---
+++ HW09_Ex002_PrintNaturalNumberMtoN/Program.cs
@@ -0,0 +1,31 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+Console.WriteLine("Введите положительное число N:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите положительное число M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+if (numberN > 0 && numberM > 0)
+{
+    Console.WriteLine($"Ряд натуральных чисел от {numberM} до {numberN}:");
+    Console.Write(PrintNaturalnumber(numberN, numberM));
+    int PrintNaturalnumber(int numN, int numM)
+    {
+        if (numberN > numberM)
+        {
+            if (numN == numM)
+                return numN;
+            Console.Write($"{PrintNaturalnumber(numN - 1, numM)}, ");
+            return numN;
+        }
+        else
+        {
+            if (numN == numM)
+                return numM;
+            Console.Write($"{PrintNaturalnumber(numN + 1, numM)}, ");
+            return numN;
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Введено не положительное число");
+}
